a8217a0bdf3651b89323ee260c8ccd7238e52c89
Landships <mcaulay.ian2@gmail.com>
2016-12-04 19:26:33 -0800

    First commit

---
+++ Landships Tutorial/Assets/Button.cs
@@ -0,0 +1,22 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+
+    public class Button : VRTK_InteractableObject
+    {
+        public float downForce;
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+            Debug.Log("pushed");
+            Debug.Log(this.gameObject);
+            this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, downForce, 0f), ForceMode.VelocityChange);
+
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+    }
+}
---
+++ Landships Tutorial/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class EndTrigger : MonoBehaviour {
+    public Text message;
+    private void Start() {
+        message.text = "Your Mission: Follow the Red Path And Collect the Yellow Ball";
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Tank")) {
+            message.text = "Mission Completed! Nice Job";
+        }
+    }
+}
